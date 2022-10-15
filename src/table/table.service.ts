88de96524b2b3a1b6fc7099f9a97528837fa33b3
CpyRubens <joaorubens2500@hotmail.com>
2022-10-15 15:43:58 -0300

    feat(table):implemente tables

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTableDto } from './dto/createTable.dto';
 import { Table } from './entities/table.entity';
 
 @Injectable()
 export class TableService {
 
-  tables:Table[]=[];
-
+  constructor(private readonly prisma: PrismaService) {}
   findAll() {
-    return this.tables;
+    return this.prisma.table.findMany();
   }
-  create(createTableDto:CreateTableDto) {
-    const table: Table={id:"random id", ...createTableDto};
-    this.tables.push(table)
-    return 'Criar uma mesa' + JSON.stringify(createTableDto)
+  create(dto: CreateTableDto) {
+    const data: Table = {  ...dto };
+
+    return this.prisma.table.create({ data });
+
   }
 }
