0bbc49a16785f52653a2eaf79cf1aa51d0f8c652
CpyRubens <joaorubens2500@hotmail.com>
2022-10-17 17:45:10 -0300

    feat(table): add FindOne

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateTableDto } from './dto/createTable.dto';
+import { Table } from './entities/table.entity';
 import { TableService } from './table.service';
 
 @ApiTags('table')
@@ -9,12 +10,25 @@ export class TableController {
   constructor(private tableService: TableService) {}
 
   @Get()
-  findAll() {
+  @ApiOperation({
+    summary:"Listar todas as mesas"
+  })
+  findAll(): Promise<Table[]> {
     return this.tableService.findAll();
+  }
+  @Get(':id')
+  @ApiOperation({
+    summary:"Listar mesa por Id"
+  })
+  findOne(@Param('id') id:string):Promise<Table>{
+    return this.tableService.findOne(id);
   }
 
   @Post()
-  create(@Body() dto: CreateTableDto) {
+  @ApiOperation({
+    summary:"Criar mesa"
+  })
+  create(@Body() dto: CreateTableDto): Promise<Table> {
     return this.tableService.create(dto);
   }
 }
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { table } from 'console';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTableDto } from './dto/createTable.dto';
 import { Table } from './entities/table.entity';
 
 @Injectable()
 export class TableService {
-
   constructor(private readonly prisma: PrismaService) {}
-  findAll() {
+  findAll(): Promise<Table[]> {
     return this.prisma.table.findMany();
   }
-  create(dto: CreateTableDto) {
-    const data: Table = {  ...dto };
+
+  findOne(id: string): Promise<Table> {
+    return this.prisma.table.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+  create(dto: CreateTableDto): Promise<Table> {
+    const data: Table = { ...dto };
 
     return this.prisma.table.create({ data });
-
   }
 }
