fb63943784d2a5f79fee8bff28e7946dce841b2f
CpyRubens <joaorubens2500@hotmail.com>
2022-10-17 21:51:08 -0300

    feat(table):implement  'delete'

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateTableDto } from './dto/createTable.dto';
 import { updateTableDto } from './dto/updateTable.dto';
@@ -12,22 +22,22 @@ export class TableController {
 
   @Get()
   @ApiOperation({
-    summary:"Listar todas as mesas"
+    summary: 'Listar todas as mesas',
   })
   findAll(): Promise<Table[]> {
     return this.tableService.findAll();
   }
   @Get(':id')
   @ApiOperation({
-    summary:"Listar mesa por Id"
+    summary: 'Listar mesa por Id',
   })
-  findOne(@Param('id') id:string):Promise<Table>{
+  findOne(@Param('id') id: string): Promise<Table> {
     return this.tableService.findOne(id);
   }
 
   @Post()
   @ApiOperation({
-    summary:"Criar mesa"
+    summary: 'Criar mesa',
   })
   create(@Body() dto: CreateTableDto): Promise<Table> {
     return this.tableService.create(dto);
@@ -35,10 +45,18 @@ export class TableController {
 
   @Patch(':id')
   @ApiOperation({
-    summary:"Editar uma mesa pelo ID"
+    summary: 'Editar uma mesa pelo ID',
   })
-  update(@Param('id')id:string, @Body()dto:updateTableDto):Promise<Table>{
+  update(@Param('id') id: string, @Body() dto: updateTableDto): Promise<Table> {
     return this.tableService.update(id, dto);
   }
 
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Excluir uma mesa pelo ID',
+  })
+  delete(@Param('id') id: string) {
+    this.tableService.delete(id);
+  }
 }
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -25,12 +25,19 @@ export class TableService {
     return this.prisma.table.create({ data });
   }
   update(id: string, dto: updateTableDto): Promise<Table> {
-
-    const data : Partial<Table> = {...dto}
+    const data: Partial<Table> = { ...dto };
 
     return this.prisma.table.update({
-      where:{id},
+      where: { id },
       data,
+    });
+  }
+
+  async delete(id: string) {
+   await this.prisma.table.delete({
+      where: {
+        id,
+      },
     });
   }
 }
