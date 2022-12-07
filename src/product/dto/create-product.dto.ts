d456fd4b3185e2c91d61b62ffe94dab059eae866
CpyRubens <joaorubens2500@hotmail.com>
2022-12-07 19:12:30 -0300

    feat(product): add class validator to create-product.dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,7 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
-  name: string
-  description: string
-  price: number
-  image: string
-  
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do produto',
+    example: 'Pizza de Calabresa',
+  })
+  name: string;
+  @IsString()
+  @ApiProperty({
+    description: 'Descricao do produto',
+    example: 'Direto de Calabria ',
+  })
+  description: string;
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @ApiProperty({
+    description: 'Preco do produto',
+    example: '49.90',
+  })
+  price: number;
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do Produto',
+    example: 'https://www.receiteria.com.br/wp-content/uploads/receitas-de-pizza-de-calabresa-6.jpg',
+  })
+  image: string;
 }
