804f54bf0336205935625ee5281304e5f841ec58
CpyRubens <joaorubens2500@hotmail.com>
2022-12-12 17:27:25 -0300

    process.env.PORT || 3333

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3333;
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
@@ -26,6 +27,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3333);
+  await app.listen(port);
+  console.log('app runing at ' + port);
 }
 bootstrap();
