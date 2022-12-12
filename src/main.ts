bc964880dd795f9aff96092bb2575625feea527e
CpyRubens <joaorubens2500@hotmail.com>
2022-12-12 18:03:29 -0300

    chore(app):improvement on status endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 3333;
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
+
+  app.set('trust proxy', 1);
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -27,7 +29,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(port);
-  console.log('app runing at ' + port);
+  await app.listen(process.env.PORT || 3333);
 }
+
 bootstrap();
